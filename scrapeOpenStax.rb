cdf367cb99f2a034b608be50641813eda0b9663c
srphillips <scott.phillips@aggiemail.usu.edu>
2017-07-03 11:46:12 -0600

    scrape by array of subjects and pull from cloudfront url

--- scrapeOpenStax.rb
+++ scrapeOpenStax.rb
@@ -1,13 +1,49 @@
-require 'mechanize'
-require 'logger'
+require 'HTTParty'
+require 'fileutils'
 
-agent = Mechanize.new
-agent.log = Logger.new "mech.log"
-agent.user_agent_alias = 'Mac Safari'
+subjects = [
+    "Prealgebra",
+    "ElementaryAlgebra",
+    "IntermediateAlgebra",
+    "CollegeAlgebra",
+    "AlgebraAndTrigonometry",
+    "Precalculus",
+    "CalculusVolume1",
+    "CalculusVolume2",
+    "CalculusVolume3",
+    "IntroductoryStatistics",
+    "AnatomyAndPhysiology",
+    "Astronomy",
+    "Biology",
+    "Concepts_Biology",
+    "Microbiology",
+    "Chemistry",
+    "ChemistryAtomsFirst",
+    "CollegePhysics",
+    "UniversityPhysicsVolume1",
+    "UniversityPhysicsVolume2",
+    "UniversityPhysicsVolume3",
+    "AmericanGovernment",
+    "PrinciplesofEconomics",
+    "PrinciplesofMacroeconomics",
+    "PrinciplesofMicroeconomics",
+    "Psychology",
+    "IntroductiontoSociology2e",
+    "U.S_History",
+    "CollegePhysicsForAPCourses",
+    "PrinciplesofMacroeconomicsforAPCourses",
+    "PrinciplesofMicroeconomicsforAPCourses",
+]
+filepath = "./openstax/"
+FileUtils.mkdir_p(filepath)
 
-page = agent.get "http://www.google.com/"
-search_form = page.form_with :name => "f"
-search_form.field_with(:name => "q").value = "Hello"
-
-search_results = agent.submit search_form
-puts search_results.body
+subjects.for_each do | topic |
+    link = "https://d3bxy9euw4e147.cloudfront.net/oscms-prodcms/media/documents/" + topic + "-OP.pdf"
+    filename = File.join(filepath, topic + ".pdf")
+    File.open(filename, "w") do |file|
+        HTTParty.get(cdn.original_cdn_url, stream_body: true) do |fragment|
+          file.write(fragment)
+        end
+    end
+    File.close(filename)
+end
